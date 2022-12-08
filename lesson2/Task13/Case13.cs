d83e0c5bb45afd074a176fada8231a44dad35ad8
Svetlana <angel_forever99@mail.ru>
2022-12-08 21:07:01 +0300

    new commit

---
+++ lesson2/Task13/Case13.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<=99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+else
+{
+    Console.WriteLine(n.ToString()[2]);
+}
+
+// пример с рандомным числом как на семинаре, только на любое число
+/*int GetRandomNumber()
+
+{
+    return new Random().Next(); //
+}
+void ShowRandomeNumber(int number)
+{
+    Console.WriteLine($"Введите трёхзначное число: {number} ");
+    string str = number.ToString();
+    Console.WriteLine($"> {str[2]} ");
+}
+
+ShowRandomeNumber(GetRandomNumber());*/
+
+
